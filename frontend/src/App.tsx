5521375fc46c9cc4c71f58640afd46d64d1fddc5
xRomory <devera.bolt21@gmail.com>
2025-07-13 14:43:38 +0800

    Added main content to Landing Page.

--- frontend/src/App.tsx
+++ frontend/src/App.tsx
@@ -1,16 +1,9 @@
 import { createBrowserRouter, RouterProvider } from "react-router-dom";
 import { QueryClientProvider } from "@tanstack/react-query";
 import { queryClient } from "./lib/queryClient";
-import { NotFound } from "./pages/NotFound";
-import LandingPage from "./pages/LandingPage";
+import { routes } from "@/routes";
 
-const router = createBrowserRouter([
-  {
-    index: true,
-    element: <LandingPage />
-  },
-  { path: "*", element: <NotFound /> }
-])
+const router = createBrowserRouter(routes);
 
 function App() {
   return (
