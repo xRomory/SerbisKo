76dd6eb5569458e1e81728e79eae2d9e648fa468
xRomory <devera.bolt21@gmail.com>
2025-07-14 13:09:54 +0800

    Backend setup

--- frontend/src/pages/Login.tsx
+++ frontend/src/pages/Login.tsx
@@ -8,15 +8,16 @@ export default function Login() {
       <main className="flex-1 flex items-center justify-center py-12">
         <div className="container mx-auto px-4 md:px-12">
           <div className="flex flex-col lg:flex-row items-center gap-6 lg:gap-12">
-            <div className="flex flex-col w-full">
+            <div className="flex flex-col lg:w-2/5 w-full">
               <LoginHeader />         
               <LoginForm />
             </div>
-            {/* <LoginForm /> */}
-            <LoginInfo />
+            <div className="w-3/5 flex items-center justify-center">
+              <LoginInfo />
+            </div>
           </div>
         </div>
       </main>
     </div>
   );
-}
+};
