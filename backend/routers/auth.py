c1f32b7406a0c07235d6effd63f04ac5619d57d4
xRomory <claneralarn@gmail.com>
2025-07-18 15:50:36 +0800

    Added Sign Up and Login endpoints

--- backend/routers/auth.py
+++ backend/routers/auth.py
@@ -15,6 +15,9 @@ def signup(data: SignUpRequest, db: Session = Depends(get_db)):
     if existing_user:
         raise HTTPException(status_code=status.HTTP_409_CONFLICT, detail="Email already registered")
     
+    if data.password != data.confirm_password:
+        raise HTTPException(status_code=400, detail="Passwords do not match")
+    
     hashed_pw = get_password_hash(data.password)
     new_user = User(
         first_name = data.first_name,
@@ -23,5 +26,68 @@ def signup(data: SignUpRequest, db: Session = Depends(get_db)):
         hashed_password = hashed_pw,
         phone_number = data.phone_number,
         address_line = data.address_line,
-        
+        region = data.region,
+        city = data.city,
+        role = data.role
     )
+    
+    db.add(new_user)
+    db.commit()
+    db.refresh(new_user)
+    
+    token = create_access_token({
+        "sub": str(new_user.public_id),
+        "role": new_user.role,
+        "email": new_user.email,
+    })
+    
+    return AuthResponse(
+        access_token=token,
+        token_type="bearer",
+        first_name=new_user.first_name,
+        last_name=new_user.last_name,
+        email=new_user.email,
+        phone_number=new_user.phone_number,
+        address_line=new_user.address_line,
+        region=new_user.region,
+        city=new_user.city,
+        role=new_user.role,
+        user_id=str(new_user.public_id),
+        expires_in=settings.ACCESS_TOKEN_EXPIRE_MINUTES * 60,
+        created_at=new_user.created_at,
+    )
+    
+@router.post("/login")
+def login(data: LoginRequest, db: Session = Depends(get_db)):
+    user = db.query(User).filter(User.email == data.email).first()
+    if not user or not verify_password(data.password, user.hashed_password):
+        raise HTTPException(status_code=status.HTTP_401_UNAUTHORIZED, detail="Invalid credentials")
+    
+    token = create_access_token({
+        "sub": str(user.public_id),
+        "role": user.role,
+        "email": user.email,
+    })
+    
+    return AuthResponse(
+        access_token=token,
+        token_type="bearer",
+        first_name=user.first_name,
+        last_name=user.last_name,
+        email=user.email,
+        phone_number=user.phone_number,
+        address_line=user.address_line,
+        region=user.region,
+        city=user.city,
+        role=user.role,
+        user_id=str(user.public_id),
+        expires_in=settings.ACCESS_TOKEN_EXPIRE_MINUTES * 60,
+        created_at=user.created_at
+    )
+    
+# Deletion of test data (Will deleted once api is connected to frontend)
+@router.delete("/delete-test-data")
+def delete_test_users(db: Session = Depends(get_db)):
+    deleted = db.query(User).filter(User.email.like("%@example.com")).delete(synchronize_session=False)
+    db.commit()
+    return {"deleted_count": deleted}
