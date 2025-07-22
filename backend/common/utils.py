f88aeb7f647250484df43f5680ceaf28aa8907bb
xRomory <claneralarn@gmail.com>
2025-07-22 19:15:59 +0800

    Added username in User model. Add User endpoints

--- backend/common/utils.py
+++ backend/common/utils.py
@@ -2,3 +2,16 @@ from datetime import datetime, timezone
 
 def utcnow():
     return datetime.now(timezone.utc)
+
+# --- Unique Username Generator ---
+import random
+import string
+from sqlalchemy.orm import Session
+from models.user import User
+
+def generate_unique_username(db: Session, prefix="serbisko_", length=8):
+    while True:
+        username = prefix + ''.join(random.choices(string.ascii_lowercase + string.digits, k=length))
+        
+        if not db.query(User).filter_by(username=username).first():
+            return username
